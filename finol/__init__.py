9441e5dd2409437fc803affa1b3f4c65541fb6af
Jiahao Li <302914353@qq.com>
2025-10-16 12:02:12 +0800

    Add files via upload

---
+++ finol/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "0.3.2"
+__author__ = "FinOL Contributors"
+
+#
+"""
+python setup.py sdist build
+twine upload dist/*
+
+pip install --upgrade finol
+python -m finol.FinOLAPPpypistats python_minor finol
+python -m finol.main.main.py
+"""
